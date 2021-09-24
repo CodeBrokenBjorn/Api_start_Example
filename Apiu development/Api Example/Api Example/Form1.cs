fa5ff8c82ed2a09834abc318e5941e693b59f230
CodeBrokenBjorn <57761349+CodeBrokenBjorn@users.noreply.github.com>
2021-09-24 10:36:51 +0100

    I spelled it wrong, yea I have soo yea.
    So this is example of how api function in C# give you understanding based in how overall api will work. Lesson was completed in 24/09/2021 second year in university.
    By CodeBrokeBjorn aka Sarunas.

---
+++ Apiu development/Api Example/Api Example/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MathsExampleLibary;
+
+namespace Api_Example
+{
+    public partial class Form1 : Form
+    {
+        private MathsExampleLibary.main myObject; //Create an object variable for the main class
+        public Form1()
+        {
+            InitializeComponent();
+            myObject = new MathsExampleLibary.main(); //Initial the libary
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = myObject.Add(double.Parse(textBox2.Text),
+               double.Parse(textBox1.Text)).ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = myObject.Subtract(double.Parse(textBox2.Text),
+                double.Parse(textBox1.Text)).ToString();
+        }
+    }
+}
